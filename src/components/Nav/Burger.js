436fc7cfae02ec90dec780c949b1acae21e34113
Joao Prates <joaoprates.az@gmail.com>
2021-11-09 17:06:47 +0000

    changing navbar menu and background colors

--- src/components/Nav/Burger.js
+++ src/components/Nav/Burger.js
@@ -16,7 +16,7 @@ const StyledBurger = styled.div`
     div {
         width: 2rem;
         height: 0.25rem;
-        background-color: ${({ open }) => open ? '#ccc' : '#468845'};
+        background-color: ${({ open }) => open ? '#fff' : '#468845'};
         border-radius: 10px;
         transform-origin: 1px;
         transition: all 0.3s linear;
