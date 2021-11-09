507b1341e3adb5be0c7d4b952f8008648842fbc6
michaelcdixon <michaelmail7@gmail.com>
2021-11-09 15:03:57 +0000

    pulling

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Home from "./components/Home";
 
 function App() {
 	return (
-		<div className="App">
+		<>
 			<Routes>
-				<Route path="/" component={Home} />
+				<Route exact path="/" element={<Home />} />
+				<Route exact path="/about" element={<About />} />
 			</Routes>
-			<About />
-		</div>
+		</>
 	);
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,70 @@
 import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+	font-size: 30px;
+	text-align: center;
+`;
+
+const Wrapper = styled.section`
+	position: absolute;
+	top: 120px;
+	right: 430px;
+	width: 500px;
+	padding: 4em;
+	border: 1px solid black;
+	border-radius: 5px;
+`;
+
+const Paragraph = styled.p``;
+
+const WhatTitle = styled.h2`
+	top: 200px;
+	left: 200px;
+`;
+
+const SubWrapper = styled.section`
+	position: absolute;
+	width: 150px;
+	top: 750px;
+	left: 200px;
+	padding: 4em;
+	border: 1px solid black;
+	border-radius: 5px;
+`;
+
+const ForTitle = styled.h2`
+	position: absolute;
+	top: 750px;
+	right: 200px;
+`;
 
 function Home() {
-	return <div>ExCode</div>;
+	return (
+		<>
+			<Wrapper>
+				<Title>The Leading Student Run Coding Bootcamp in the UK</Title>
+				<Paragraph>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Facilis, esse cupiditate. Quas, praesentium asperiores? Ea,
+					voluptates provident dicta enim nostrum facilis commodi quas
+					illum officiis libero quod sequi nemo error.
+				</Paragraph>
+			</Wrapper>
+
+			<SubWrapper>
+				<WhatTitle>Why Apply?</WhatTitle>
+				<Paragraph>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Consequuntur corrupti, ducimus, qui iste incidunt
+					perspiciatis saepe asperiores commodi magni beatae quis odit
+					dicta blanditiis officiis recusandae id assumenda! Unde,
+					dignissimos?
+				</Paragraph>
+			</SubWrapper>
+			<ForTitle>For Whom?</ForTitle>
+		</>
+	);
 }
 
 export default Home;
