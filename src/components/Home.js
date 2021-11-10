f247d070bdccebf77d9274488273e16d06119b07
Joao Prates <joaoprates.az@gmail.com>
2021-11-10 17:06:54 +0000

    fixing main page

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,6 +25,8 @@ const R2 = styled.section`
   	flex-direction: column;
   	align-items: center;
   	justify-content: center;
+	padding-top: 150px;
+	padding-bottom: 150px;
 `;
 
 const R3 = styled.section`
@@ -35,6 +37,9 @@ const R3 = styled.section`
   	align-items: center;
   	justify-content: center;
 `;
+
+const BracketLanding1 = styled.div`
+	display: flex;`
 
 const Signup = styled.h2`
 	color: #fff;
@@ -55,19 +60,28 @@ const SubTitle = styled.h2`
 	font-family: 'Ubuntu', sans-serif;
 `;
 
-const Bracket1 = styled.h1 `
+const Bracket1 = styled.h1`
 
 color: #62b22f;
-font-size: 150px;
+font-size: 170px;
 font-family: 'Ubuntu', sans-serif;
+position: absolute;
+left: 175px;
+
+div {
+	position: absolute;
+    bottom: -180px;
+}
 
 `;
 
-const Bracket2 = styled.h1 `
+const Bracket2 = styled.h1`
 
 color: #62b22f;
 font-size: 150px;
 font-family: 'Ubuntu', sans-serif;
+position: absolute;
+right: 175px;
 
 
 
@@ -92,51 +106,61 @@ function Home() {
 				</div>
 
 			</R1>
-			
 
-		
-			<R2 className = "row-landing-sec2">
-			  <div className="col-md-8">
-			  <Bracket1><div 
-      dangerouslySetInnerHTML={{__html: "{"}}
-    /></Bracket1>
-				<SubTitle>What's ExCode:</SubTitle>
-				<Paragraph>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Consequuntur corrupti, ducimus, qui iste incidunt
-					perspiciatis saepe asperiores commodi magni beatae quis odit
-					dicta blanditiis officiis recusandae id assumenda! Unde,
-					dignissimos?
-				</Paragraph>
-				 
-	
-			
-			<SubTitle>Why Apply:</SubTitle>
-				<Paragraph>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Consequuntur corrupti, ducimus, qui iste incidunt
-					perspiciatis saepe asperiores commodi magni beatae quis odit
-					dicta blanditiis officiis recusandae id assumenda! Unde,
-					dignissimos?
-				</Paragraph>
-				
-		
-			<SubTitle>For Whom:</SubTitle>
-				<Paragraph>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Consequuntur corrupti, ducimus, qui iste incidunt
-					perspiciatis saepe asperiores commodi magni beatae quis odit
-					dicta blanditiis officiis recusandae id assumenda! Unde,
-					dignissimos?
-				</Paragraph>
-				<Bracket2><div 
-      dangerouslySetInnerHTML={{__html: "}"}}
-    /></Bracket2>
+
+
+			<R2 className="row-landing-sec2">
+				<div style={{ paddingLeft: '250px', paddingRight: '250px' }}>
+
+						<BracketLanding1>
+							<Bracket1><div
+								dangerouslySetInnerHTML={{ __html: "{" }}
+							/></Bracket1>
+							<div>
+								<SubTitle>What's ExCode:</SubTitle>
+								<div>
+									<Paragraph>
+										Lorem ipsum dolor sit amet consectetur adipisicing elit.
+										Consequuntur corrupti, ducimus, qui iste incidunt
+										perspiciatis saepe asperiores commodi magni beatae quis odit
+										dicta blanditiis officiis recusandae id assumenda! Unde,
+										dignissimos?
+									</Paragraph>
+								</div>
+							</div>
+						</BracketLanding1>
+
+
+
+						<SubTitle>Why Apply:</SubTitle>
+						<Paragraph>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Consequuntur corrupti, ducimus, qui iste incidunt
+							perspiciatis saepe asperiores commodi magni beatae quis odit
+							dicta blanditiis officiis recusandae id assumenda! Unde,
+							dignissimos?
+						</Paragraph>
+
+						<BracketLanding1>
+							<div>
+								<SubTitle>For Whom:</SubTitle>
+								<Paragraph>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Consequuntur corrupti, ducimus, qui iste incidunt
+									perspiciatis saepe asperiores commodi magni beatae quis odit
+									dicta blanditiis officiis recusandae id assumenda! Unde,
+									dignissimos?
+								</Paragraph>
+							</div>
+							<Bracket2><div
+								dangerouslySetInnerHTML={{ __html: "}" }}
+							/></Bracket2>
+						</BracketLanding1>
 				</div>
 			</R2>
 
 			<R3 className="row-landing-sec3">
-				<div className="col-md-6">
+				<div>
 					<Signup>Want to join the Bootcamp?</Signup>
 					<NavLink to="/program">
 						<Button className="program-button" variant="outline-success" href="/program" size="lg">Program</Button>{' '}
