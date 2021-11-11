aaec04955027df192cd19c27ce893409ce1ec5f9
Joao Prates <joaoprates.az@gmail.com>
2021-11-11 20:05:58 +0000

    more responsiveness 2

--- src/components/Program.js
+++ src/components/Program.js
@@ -6,15 +6,14 @@ const Banner = styled.div`
 	background-image: url("course.png");
 	background-repeat: no-repeat;
 	background-size: 100%;
-	height: 75vh;
-	margin: 50px 0px 50px 0px;
+
 `;
 
 
 const BannerH1 = styled.h1`
 	font-family: 'Raleway', sans-serif, bold ;
 	font-style: bold;
-	font-size: 9vw;
+	font-size: 8vw;
 `;
 
 function Program() {
